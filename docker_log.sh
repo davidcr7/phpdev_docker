830a0dbef8de1bb4fbecd88f8263c7f719ee1003
System Administrator <root@DavidcrdeMacBook-Pro.local>
2018-01-30 17:26:46 +0800

    添加注释

--- docker_log.sh
+++ docker_log.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
 logString=""
+#log时间
 date=`date +%Y-%m-%d%h:%M:%S`
+
+#拼接log信息
 for((i=1;i<=$#;i++ ));
 do
 	logString=$logString" "${!i}
 done
 logString=$date":"$logString
 logFile="/tmp/docker_error.log" 
+
+#追加log到文件
 echo $logString >> $logFile
